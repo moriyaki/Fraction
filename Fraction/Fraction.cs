91f8b3b731165779533567d05668ac04863cd597
moriyaki <moriyaki999@gmail.com>
2020-09-27 10:18:06 +0900

    小数点対応

--- Fraction/Fraction.cs
+++ Fraction/Fraction.cs
@@ -5,131 +5,155 @@ using System.Text;
 
 namespace moriyaki.fraction
 {
-    public readonly struct Fraction
-    {
-        private readonly int num;
-        private readonly int den;
+	public  struct Fraction
+	{
+		private  int num;
+		private  int den;
 
-        public Fraction(int numerator, int denominator = 1)
-        {
-            if (denominator == 0)
-            {
-                throw new ArgumentException("Denominator cannot be zero", nameof(denominator));
-            }
-            num = numerator;
-            den = denominator;
-        }
+		public Fraction(int numerator, int denominator = 1)
+		{
+			if (denominator == 0)
+			{
+				throw new ArgumentException("Denominator cannot be zero", nameof(denominator));
+			}
+			num = numerator;
+			den = denominator;
+		}
 
-        // 正の数
-        public static Fraction operator +(Fraction value) => value;
+		/// <summary>
+		/// 小数文字で受け取り
+		/// </summary>
+		/// <param name="decString"></param>
+		public Fraction(string decString)
+		{
+			var numbers = decString.Split('.');
+			if (numbers.Length > 2) throw new InvalidOperationException();
 
-        // 負の数
-        public static Fraction operator -(Fraction a) => new Fraction(-a.num, a.den);
+			int numbase, dec;
 
+			if (!Int32.TryParse(numbers[0], out numbase))
+			{
+				throw new InvalidOperationException();
+			}
+			if (!Int32.TryParse(numbers[1], out dec))
+			{
+				throw new InvalidOperationException();
+			}
+			den = (int)(Math.Pow(10, numbers[1].Length));
+			num = numbase * den + dec;
+		}
 
-        public static implicit operator int(Fraction a) => a.num;
+		// 正の数
+		public static Fraction operator +(Fraction value) => value;
 
-        // 加算
-        // a/b + c/d = (a*c + b*d) / b*d な公式 
-        public static Fraction operator +(Fraction a, Fraction b) => new Fraction(a.num * b.den + b.num * a.den, a.den * b.den);
-
-        // a/b + c = (a/b + bc/b) = (a+bc / b)
-        public static Fraction operator +(Fraction a, int b) => new Fraction(a.num + b * a.den, a.den);
-
-        // 減算
-        // a/b - c = (a-bc / b)
-        public static Fraction operator -(Fraction a, int b) => new Fraction(a.num - b * a.den, a.den);
-
-        // 乗算
-        public static Fraction operator *(Fraction a, Fraction b) => new Fraction(a.num * b.num, a.den * b.den);
-
-        // a/b * c = ac / b
-        public static Fraction operator *(Fraction a, int c) => new Fraction(a.num * c, a.den);
-
-        // インクリメント
-        public static Fraction operator ++(Fraction a) => new Fraction(a.num + a.den, a.den);
-
-        // デクリメント
-        public static Fraction operator --(Fraction a) => new Fraction(a.num - a.den, a.den);
-
-        // 除算
-        // a/b / cd = ad/bc
-        public static Fraction operator /(Fraction a, Fraction b)
-        {
-            if (b.num == 0)
-            {
-                throw new DivideByZeroException();
-            }
-            return new Fraction(a.num * b.den, a.den * b.num);
-        }
-
-        // a/b / c = a / bc
-        public static Fraction operator /(Fraction a, int c) => new Fraction(a.num, a.den * c);
+		// 負の数
+		public static Fraction operator -(Fraction a) => new Fraction(-a.num, a.den);
 
 
-        // 最大公約数の獲得(ユークリッド互除法)
-        public static int Gcd(int a, int b)
-        {
-            int abs_a = (a > 0) ? a : -a;
-            int abs_b = (b > 0) ? b : -b;
+		public static implicit operator int(Fraction a) => a.num;
 
-            if (abs_a < abs_b)
-                return Gcd(b, a);
+		// 加算
+		// a/b + c/d = (a*c + b*d) / b*d な公式 
+		public static Fraction operator +(Fraction a, Fraction b) => new Fraction(a.num * b.den + b.num * a.den, a.den * b.den);
 
-            while (abs_b != 0)
-            {
-                var remainder = abs_a % abs_b;
-                abs_a = abs_b;
-                abs_b = remainder;
-            }
-            return abs_a;
-        }
+		// a/b + c = (a/b + bc/b) = (a+bc / b)
+		public static Fraction operator +(Fraction a, int b) => new Fraction(a.num + b * a.den, a.den);
 
-        public override string ToString()
-        {
-            int numerator = num;
-            int denominator = den;
+		// 減算
+		// a/b - c = (a-bc / b)
+		public static Fraction operator -(Fraction a, int b) => new Fraction(a.num - b * a.den, a.den);
 
-            int gcd = Gcd(num, den);
-            if (gcd != 0)
-            {
-                numerator /= gcd;
-                denominator /= gcd;
-            }
+		// 乗算
+		public static Fraction operator *(Fraction a, Fraction b) => new Fraction(a.num * b.num, a.den * b.den);
 
-            if (denominator == 1)
-            {
-                // 分母1なら整数表示
-                return $"{numerator}";
-            }
-            else
-            {
-                return $"{numerator} / {denominator}";
-            }
-        }
+		// a/b * c = ac / b
+		public static Fraction operator *(Fraction a, int c) => new Fraction(a.num * c, a.den);
 
-        /// <summary>
-        /// 整数＋真分数にする
-        /// </summary>
-        /// <returns>整数と真分数のタプル</returns>
-        public (int, Fraction) GetProperFraction()
-        {
-            if (num < den)
-            {
-                return (0, new Fraction(num, den));
-            }
-            else
-            {
-                int k = num % den;
-                int n = num - k * den;
-                return (k, new Fraction(n, den));
-            }
-        }
+		// インクリメント
+		public static Fraction operator ++(Fraction a) => new Fraction(a.num + a.den, a.den);
 
-        /// <summary>
-        /// 小数点で取得
-        /// </summary>
-        public double Double { get { return (double)num / den; } }
-    }
+		// デクリメント
+		public static Fraction operator --(Fraction a) => new Fraction(a.num - a.den, a.den);
+
+
+		// 除算
+		// a/b / cd = ad/bc
+		public static Fraction operator /(Fraction a, Fraction b)
+		{
+			if (b.num == 0)
+			{
+				throw new DivideByZeroException();
+			}
+			return new Fraction(a.num * b.den, a.den * b.num);
+		}
+
+		// a/b / c = a / bc
+		public static Fraction operator /(Fraction a, int c) => new Fraction(a.num, a.den * c);
+
+
+		// 最大公約数の獲得(ユークリッド互除法)
+		public static int Gcd(int a, int b)
+		{
+			int abs_a = (a > 0) ? a : -a;
+			int abs_b = (b > 0) ? b : -b;
+
+			if (abs_a < abs_b)
+				return Gcd(b, a);
+
+			while (abs_b != 0)
+			{
+				var remainder = abs_a % abs_b;
+				abs_a = abs_b;
+				abs_b = remainder;
+			}
+			return abs_a;
+		}
+
+		public override string ToString()
+		{
+			int numerator = num;
+			int denominator = den;
+
+			int gcd = Gcd(num, den);
+			if (gcd != 0)
+			{
+				numerator /= gcd;
+				denominator /= gcd;
+			}
+
+			if (denominator == 1)
+			{
+				// 分母1なら整数表示
+				return $"{numerator}";
+			}
+			else
+			{
+				return $"{numerator} / {denominator}";
+			}
+		}
+
+		/// <summary>
+		/// 整数＋真分数にする
+		/// </summary>
+		/// <returns>整数と真分数のタプル</returns>
+		public (int, Fraction) GetProperFraction()
+		{
+			if (num < den)
+			{
+				return (0, new Fraction(num, den));
+			}
+			else
+			{
+				int k = num / den;
+				int n = num % den;
+				return (k, new Fraction(n, den));
+			}
+		}
+
+		/// <summary>
+		/// 小数点で取得
+		/// </summary>
+		public double Double { get { return (double)num / den; } }
+	}
 
 }
--- Fraction/Program.cs
+++ Fraction/Program.cs
@@ -1,61 +1,69 @@
 ﻿using System;
+using System.Globalization;
 using moriyaki.fraction;
 
 namespace FractionTest
 {
-    class Program
-    {
-        public static void Main(string[] args)
-        {
-            var a = new Fraction(5, 4);
-            var b = new Fraction(2, 3);
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			/*
+						var a = new Fraction(5, 4);
+						var b = new Fraction(2, 3);
 
-            FractionOperationTest(a, b);
-/*
-            FractionIntOperationTest(a, 2);
-            FractionIntOperationTest(b, 3);
-*/
-        }
+						FractionOperationTest(a, b);
+						FractionIntOperationTest(a, 2);
+						FractionIntOperationTest(b, 3);
+			*/
+			var a_str = "392.437";
+			var a = new Fraction(a_str);
+			var b = new Fraction(2, 3);
 
-        public static void FractionOperationTest(Fraction a, Fraction b)
-        {
-            Console.WriteLine("-------------------");
-            Console.WriteLine("FractionOperation");
-            Console.WriteLine("a = " + a);
-            Console.WriteLine("b = " + b);
-            Console.WriteLine("");
-            Console.WriteLine("-a = " + -a);
-            Console.WriteLine("a+b = " + (a + b));
-            Console.WriteLine("a-b = " + (a - b));
-            Console.WriteLine("a*b = " + (a * b));
-            Console.WriteLine("a/b = " + (a / b));
-            Console.WriteLine("++a : " + ++a);
-            Console.WriteLine("--a : " + --a);
+			Console.WriteLine($"a_string is {a_str}");
+			FractionOperationTest(a, b);
+			
+		}
 
-            int k;
-            Fraction p;
+		public static void FractionOperationTest(Fraction a, Fraction b)
+		{
+			Console.WriteLine("-------------------");
+			Console.WriteLine("FractionOperation");
+			Console.WriteLine($"a = {a}");
+			Console.WriteLine($"b = {b}");
+			Console.WriteLine();
+			Console.WriteLine("-a =  " + -a);
+			Console.WriteLine("a+b = " + (a + b));
+			Console.WriteLine("a-b = " + (a - b));
+			Console.WriteLine("a*b = " + (a * b));
+			Console.WriteLine("a/b = " + (a / b));
+			Console.WriteLine("++a : " + ++a);
+			Console.WriteLine("--a : " + --a);
 
-            (k, p) = a.GetProperFraction();
-            Console.WriteLine("a = " + k + " + (" + p + ")");
-            (k, p) = b.GetProperFraction();
-            Console.WriteLine("b = " + k + " + (" + p + ")");
-            Console.WriteLine("a = " + a.Double);
-            Console.WriteLine("b = " + b.Double);
-        }
+			int k;
+			Fraction p;
 
-        public static void FractionIntOperationTest(Fraction a, int b)
-        {
-            Console.WriteLine("-------------------");
-            Console.WriteLine("FractionIntOperation");
-            Console.WriteLine("a = " + a);
-            Console.WriteLine("b = " + b);
-            Console.WriteLine("");
-            Console.WriteLine("-a = " + -a);
-            Console.WriteLine("a+b = " + (a + b));
-            Console.WriteLine("a-b = " + (a - b));
-            Console.WriteLine("a*b = " + (a * b));
-            Console.WriteLine("a/b = " + (a / b));
+			(k, p) = a.GetProperFraction();
+			Console.WriteLine("a = " + k + " + (" + p + ")");
+			(k, p) = b.GetProperFraction();
+			Console.WriteLine("b = " + k + " + (" + p + ")");
+			Console.WriteLine("a = " + a.Double);
+			Console.WriteLine("b = " + b.Double);
+		}
 
-        }
-    }
+		public static void FractionIntOperationTest(Fraction a, int b)
+		{
+			Console.WriteLine("-------------------");
+			Console.WriteLine("FractionIntOperation");
+			Console.WriteLine($"a = {a}");
+			Console.WriteLine($"b = {b}");
+			Console.WriteLine("");
+			Console.WriteLine("-a = " + -a);
+			Console.WriteLine("a+b = " + (a + b));
+			Console.WriteLine("a-b = " + (a - b));
+			Console.WriteLine("a*b = " + (a * b));
+			Console.WriteLine("a/b = " + (a / b));
+
+		}
+	}
 }
